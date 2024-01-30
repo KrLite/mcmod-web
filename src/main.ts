c387d00d50a427ea2baa567be572674c99fb6750
KrLite <68179735+KrLite@users.noreply.github.com>
2024-01-30 19:41:48 +0800

    fundamentals

--- src/main.ts
+++ src/main.ts
@@ -8,4 +8,25 @@ const app = createApp(App)
 
 app.use(router)
 
+import { createHead, useHead } from '@unhead/vue'
+
+const head = createHead()
+useHead({
+  title: '主页',
+  titleTemplate: '%s - MC百科',
+  meta: [
+    {
+      name: 'description',
+      content:
+        '最大的Minecraft中文Mod百科'
+    },
+    {
+      name: 'keywords',
+      content:
+        'minecraft, mod, game, wiki'
+    }
+  ]
+})
+app.use(head)
+
 app.mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+		path: '/:error*',
+		name: '404',
+		component: () => import('@/views/404View.vue')
+	},
     {
       path: '/',
       name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/HomeView.vue')
     }
   ]
 })
