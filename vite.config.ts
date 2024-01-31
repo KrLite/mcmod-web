fa50e3ae310ff21299fab60c90decf810ccf4ee0
KrLite <68179735+KrLite@users.noreply.github.com>
2024-01-31 19:44:40 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/Web.mcmod.cn/',
+  base: 'https://krlite.github.io/Web.mcmod.cn/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
